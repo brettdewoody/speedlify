82564303c0f9c6605d829e64a1638bab19187d2e
Brett DeWoody <brett@firstbasehq.com>
2022-07-14 15:52:21 +0100

    Add websites.

---
+++ _data/sites/birkscinema.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Birks Cinema", // optional, falls back to object key
+	description: "",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://birkscinema.co.uk"
+	]
+};
---
+++ _data/sites/chilliesaberfeldy.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: "Chillies Aberfeldy", // optional, falls back to object key
+	description: "",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://chillies-aberfeldy-website.netlify.app/"
+	]
+};
