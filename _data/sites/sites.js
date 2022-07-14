54a25b2aaa0fd53051a14267991b3a291ea62a32
Brett DeWoody <brett@firstbasehq.com>
2022-07-14 16:02:53 +0100

    Update sites.

---
+++ _data/sites/sites.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Portfolio Sites",
+	description: "",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://birkscinema.co.uk",
+		"https://chillies-aberfeldy-website.netlify.app/",
+		"https://ruraljobsscotland.co.uk",
+	]
+};
